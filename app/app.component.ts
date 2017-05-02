22d4f6e99118f7a1462705405ab751d6c0c5d65a
Chris Carr and Megan Warnock <chris@>
2017-05-02 11:24:57 -0700

    add data down, actions up

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,7 +8,7 @@ import { Task } from './task.model';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
 
-    <task-list></task-list>
+    <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
 
     <hr>
        <div>
@@ -37,6 +37,12 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   selectedTask = null;
+
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
