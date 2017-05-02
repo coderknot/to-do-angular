012a3c7625f2ffee64cb022c6c81cd1648eed284
Megan Warnock <megandwarnock@gmail.com>
2017-05-01 20:56:54 -0700

    add conditional directive to hide/show form

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,18 +11,21 @@ import { Component } from '@angular/core';
     </ul>
     <hr>
        <div>
-        <h3>{{selectedTask.description}}</h3>
-        <p>Task Complete? {{selectedTask.done}}</p>
-       <h3>Edit Task</h3>
-       <label>Enter Task Description:</label>
-       <input [(ngModel)]="selectedTask.description">
-        <label>Enter Task Priority (1-3):</label>
-        <br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-       </div>
-     </div>
+        <div *ngIf="selectedTask">
+          <h3>{{selectedTask.description}}</h3>
+          <p>Task Complete? {{selectedTask.done}}</p>
+          <h3>Edit Task</h3>
+          <label>Enter Task Description:</label>
+          <input [(ngModel)]="selectedTask.description">
+          <label>Enter Task Priority (1-3):</label>
+          <br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+          <button (click)="finishedEditing()">Done</button>
+        </div>
+      </div>
+  </div>
    `
 })
 
@@ -37,7 +40,7 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-selectedTask: Task = this.tasks[0];
+selectedTask = null;
 
     editTask(clickedTask) {
       this.selectedTask = clickedTask;
@@ -60,6 +63,10 @@ selectedTask: Task = this.tasks[0];
       return "bg-info";
     }
   }
+
+  finishedEditing() {
+   this.selectedTask = null;
+ }
 }
 
 export class Task {
