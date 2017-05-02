3d9be0a2db6bf7c32bc3237f1b750566678030f0
Chris Carr and Megan Warnock <megan@>
2017-05-02 16:22:36 -0700

    add task pip and select menu

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -4,8 +4,18 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template: `
+  <select (change)="onChange($event.target.value)">
+    <option value="allTasks">All Tasks</option>
+    <option value="completedTasks">Completed Tasks</option>
+    <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+  </select>
+
   <ul>
-  <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList">{{currentTask.description}} <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button></li>
+    <li [class]="priorityColor(currentTask)"  *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
+      <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
+      <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
+      <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button>
+    </li>
   </ul>
   `
 })
@@ -18,13 +28,13 @@ export class TaskListComponent {
     this.clickSender.emit(taskToEdit);
   }
 
-  isDone(clickedTask: Task) {
-    if(clickedTask.done === true) {
-      alert("This task is done!");
-    } else {
-      alert("This task is not done. Better get to work!");
-    }
-  }
+  // isDone(clickedTask: Task) {
+  //   if(clickedTask.done === true) {
+  //     alert("This task is done!");
+  //   } else {
+  //     alert("This task is not done. Better get to work!");
+  //   }
+  // }
 
   priorityColor(currentTask){
     if (currentTask.priority === 3){
@@ -34,5 +44,15 @@ export class TaskListComponent {
     } else {
       return "bg-info";
     }
+  }
+
+  filterByCompleteness: string = "incompleteTasks";
+
+  onChange(optionFromMenu) {
+    this.filterByCompleteness = optionFromMenu;
+  }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+    clickedTask.done = setCompleteness;
   }
 }
